f2731b1de530f517b9f9af16766d101c231913ef
Guilherme <guilhermegm.andrade@gmail.com>
2022-06-12 02:15:00 -0300

    atualizando novas informações / populando tabelas

--- api/models/turmas.js
+++ api/models/turmas.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Turmas.hasMany(models.Matriculas, {foreignKey: 'turma_id'});
+      Turmas.belongsTo(models.Pessoas, {foreignKey: 'docente_id'});
+      Turmas.belongsTo(models.Níveis, {foreignKey: 'nivel_id'});
     }
   }
   Turmas.init({
